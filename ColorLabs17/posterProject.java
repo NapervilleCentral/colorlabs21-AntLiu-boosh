bbfd20da98b164280766721ef11eec07da18c82d
Anthony Liu <abliu1@stu.naperville203.org>
2026-05-05 11:49:48 -0500

    5.5.2026 update

--- ColorLabs17/posterProject.java
+++ ColorLabs17/posterProject.java
@@ -13,22 +13,28 @@ public class posterProject
         
         
         Picture schizo = new Picture("images/Schizo3.jpg");
-        //Picture canvas = new Picture("images/canvas.jpg");
+        Picture schizo1 = new Picture("images/Schizo3.jpg");
+        Picture schizo2 = new Picture("images/Schizo3.jpg");
+        Picture schizo3 = new Picture("images/Schizo3.jpg");
+        Picture loss = new Picture("images/Loss_comic.jpg");
+        Picture canvas = new Picture("images/canvas.jpg");
         Picture temple = new Picture("images/temple.jpg");
         Pixel[] ratpix;
         ratpix = schizo.getPixels();
-        //copyToCanvas(schizo, canvas, 0, 0);
-        //mirrorY(schizo);
-       // copyToCanvas(schizo, canvas, 2000, 0);
-        
+        copyToCanvas(schizo, canvas, 0, 0);
+        mirrorY(schizo);
+        copyToCanvas(schizo, canvas, 2000, 0);
+        blend(schizo1, loss);
+        copyToCanvas(schizo1, canvas, 4000, 0);
+        /*
         Pixel[] tempix;
         tempix = temple.getPixels();
         temple.explore();
         
-        mirrorY(temple, 20, 100, 10, 540);
+        mirrorY(temple, 20, 100, 10, 550);
         temple.explore();
-        
-        //canvas.explore();
+        */
+        canvas.explore();
         //
         
     }
@@ -46,16 +52,29 @@ public class posterProject
             }
         }
     }
+    
     public static void mirrorY(Picture source, int y1, int y2, int x1, int x2){
         int width = x2 - x1;
         int mirrorPoint = width/2;
         Pixel leftPixel = null;
         Pixel rightPixel = null;
-        for (int col = x1; col < x2; col++){
-            for (int row = y1; row < y2; row++){
-                leftPixel = source.getPixel(y1 + row,  x1 + col);
-                rightPixel = source.getPixel(y1 + width - 1 - row, x1 + col);
+        for (int col = x1; col < y2; col++){
+            for (int row = y1; row < mirrorPoint; row++){
+                leftPixel = source.getPixel(x1 + row,  y1 + col);
+                rightPixel = source.getPixel(x2 - 1 - row, y1 + col);
                 rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+    }
+    
+    public static void blend(Picture source, Picture overlay){
+        Pixel sourcePixel = null;
+        Pixel overlayPixel = null;
+        for (double col = 0; col < source.getHeight(); col += 2){
+            for (double row = 1; row < source.getWidth() - 1; row += 3){
+                sourcePixel = source.getPixel((int)row, (int)col);
+                overlayPixel = overlay.getPixel((int)row, (int)col);
+                sourcePixel.setColor(overlayPixel.getColor());
             }
         }
     }
@@ -65,6 +84,23 @@ public class posterProject
         Pixel targetPix = null;
         for (int sourceX = 0, targetX = holdX; sourceX < source.getWidth(); sourceX++, targetX++){
             for (int sourceY = 0, targetY = holdY; sourceY < source.getHeight(); sourceY++, targetY++){
+                sourcePix = source.getPixel(sourceX, sourceY);
+                targetPix = target.getPixel(targetX, targetY);
+                targetPix.setColor(sourcePix.getColor());
+            }
+        }
+    }
+    
+    public static void RECCompress(Picture source, Picture target, int holdX, int holdY){
+        if (source.getHeight() <= 20){
+            return; 
+        }
+        
+        Pixel sourcePix = null;
+        Pixel targetPix = null;
+        
+        for (int sourceX = 0, targetX = holdX; sourceX < source.getWidth(); sourceX++, targetX++){
+            for (int sourceY = 0, targetY = holdY; sourceY < source.getHeight(); sourceY+=2, targetY++){
                 sourcePix = source.getPixel(sourceX, sourceY);
                 targetPix = target.getPixel(targetX, targetY);
                 targetPix.setColor(sourcePix.getColor());
